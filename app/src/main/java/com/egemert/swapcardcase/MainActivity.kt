b5f5575a2cde91d5e48f30619f0314dd8327642e
egemert <egemert1203@gmail.com>
2025-08-23 16:52:47 +0300

    AppModule provided.

--- app/src/main/java/com/egemert/swapcardcase/MainActivity.kt
+++ app/src/main/java/com/egemert/swapcardcase/MainActivity.kt
@@ -7,13 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.egemert.swapcardcase.screen.UserListScreen
 import com.egemert.swapcardcase.ui.theme.SwapCardCaseTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
