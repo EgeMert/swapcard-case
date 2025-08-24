4547102bc821b8cb507614e8ea291bbe8882ca59
egemert <egemert1203@gmail.com>
2025-08-24 16:11:21 +0300

    Deleted unused function call

--- app/src/main/java/com/egemert/swapcardcase/viewmodel/UserListViewModel.kt
+++ app/src/main/java/com/egemert/swapcardcase/viewmodel/UserListViewModel.kt
@@ -70,7 +70,6 @@ class UserListViewModel @Inject constructor(
             } else {
                 userRepository.addBookmark(user)
             }
-            loadBookmarkedUsers()
         }
     }
 
