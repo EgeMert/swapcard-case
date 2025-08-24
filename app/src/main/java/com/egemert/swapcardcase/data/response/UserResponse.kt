cad00aabf22e7eeda5739d0b8c56eedb528a0e6a
egemert <egemert1203@gmail.com>
2025-08-24 14:57:28 +0300

    Deleted unused fields from response

--- app/src/main/java/com/egemert/swapcardcase/data/response/UserResponse.kt
+++ app/src/main/java/com/egemert/swapcardcase/data/response/UserResponse.kt
@@ -10,18 +10,12 @@ data class UserResponse(
 )
 
 data class User(
-    @SerializedName("gender") val gender: String? = null,
     @SerializedName("name") val name: Name? = null,
     @SerializedName("location") val location: Location? = null,
     @SerializedName("email") val email: String? = null,
     @SerializedName("login") val login: Login? = null,
-    @SerializedName("dob") val dob: DateOfBirth? = null,
-    @SerializedName("registered") val registered: Registered? = null,
-    @SerializedName("phone") val phone: String? = null,
-    @SerializedName("cell") val cell: String? = null,
     @SerializedName("id") val id: Id? = null,
-    @SerializedName("picture") val picture: Picture? = null,
-    @SerializedName("nat") val nationality: String? = null
+    @SerializedName("picture") val picture: Picture? = null
 )
 
 data class Name(
@@ -31,47 +25,11 @@ data class Name(
 )
 
 data class Location(
-    @SerializedName("street") val street: Street? = null,
-    @SerializedName("city") val city: String? = null,
-    @SerializedName("state") val state: String? = null,
-    @SerializedName("country") val country: String? = null,
-    @SerializedName("coordinates") val coordinates: Coordinates? = null,
-    @SerializedName("timezone") val timezone: Timezone? = null
-)
-
-data class Street(
-    @SerializedName("number") val number: Int? = null,
-    @SerializedName("name") val name: String? = null
-)
-
-data class Coordinates(
-    @SerializedName("latitude") val latitude: String? = null,
-    @SerializedName("longitude") val longitude: String? = null
-)
-
-data class Timezone(
-    @SerializedName("offset") val offset: String? = null,
-    @SerializedName("description") val description: String? = null
+    @SerializedName("country") val country: String? = null
 )
 
 data class Login(
-    @SerializedName("uuid") val uuid: String? = null,
-    @SerializedName("username") val username: String? = null,
-    @SerializedName("password") val password: String? = null,
-    @SerializedName("salt") val salt: String? = null,
-    @SerializedName("md5") val md5: String? = null,
-    @SerializedName("sha1") val sha1: String? = null,
-    @SerializedName("sha256") val sha256: String? = null
-)
-
-data class DateOfBirth(
-    @SerializedName("date") val date: String? = null,
-    @SerializedName("age") val age: Int? = null
-)
-
-data class Registered(
-    @SerializedName("date") val date: String? = null,
-    @SerializedName("age") val age: Int? = null
+    @SerializedName("uuid") val uuid: String? = null
 )
 
 data class Id(
@@ -80,9 +38,7 @@ data class Id(
 )
 
 data class Picture(
-    @SerializedName("large") val large: String? = null,
-    @SerializedName("medium") val medium: String? = null,
-    @SerializedName("thumbnail") val thumbnail: String? = null
+    @SerializedName("large") val large: String? = null
 )
 
 data class Info(
