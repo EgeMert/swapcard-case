40b88b47aced2c9bf1f0a27d05064d791e49a110
egemert <egemert1203@gmail.com>
2025-08-24 12:20:12 +0300

    ErrorResponse handled in safe call

--- app/src/main/java/com/egemert/swapcardcase/data/error/ApiErrorResponse.kt
+++ app/src/main/java/com/egemert/swapcardcase/data/error/ApiErrorResponse.kt
@@ -2,7 +2,7 @@ package com.egemert.swapcardcase.data.error
 
 import com.google.gson.annotations.SerializedName
 
-data class ErrorResponse(
+data class ApiErrorResponse(
     @SerializedName("error")
     val error: String? = null
 )
